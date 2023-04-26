e68f50225076ab17ac0a9c0d51dff802fd9c7b57
VoltStudent <volt251988@gmail.com>
2023-04-26 20:19:10 +0300

    made task 4

--- task-3/script-3.js
+++ task-3/script-3.js
@@ -1,168 +1,168 @@
-//1
-const btn1 = document.querySelector(".btn1");
-function createArray(start, end) {
-    let arr = [];
-    for (let i = start; i <= end; ++i) {
-        arr.push(i);
-    }
-    return arr;
-}
-btn1.addEventListener('click', () => {
-    start = +document.querySelector(".start").value;
-    end = +document.querySelector(".end").value;
-    console.log(createArray(start, end));
-});
+// //1
+// const btn1 = document.querySelector(".btn1");
+// function createArray(start, end) {
+//     let arr = [];
+//     for (let i = start; i <= end; ++i) {
+//         arr.push(i);
+//     }
+//     return arr;
+// }
+// btn1.addEventListener('click', () => {
+//     start = +document.querySelector(".start").value;
+//     end = +document.querySelector(".end").value;
+//     console.log(createArray(start, end));
+// });
 
-//2
-const btn2 = document.querySelector(".btn2");
-function numbers(a, b) {
-    let string = "";
-    for (let i = a; i <= b; ++i) {
-        for (let j = 0; j < i - a + 1; ++j) {
-            string += i + ",";
-        }
-    }
-    return string;
-}
-btn2.addEventListener('click', () => {
-    a = +document.querySelector(".a").value;
-    b = +document.querySelector(".b").value;
-    console.log(numbers(a, b));
-});
+// //2
+// const btn2 = document.querySelector(".btn2");
+// function numbers(a, b) {
+//     let string = "";
+//     for (let i = a; i <= b; ++i) {
+//         for (let j = 0; j < i - a + 1; ++j) {
+//             string += i + ",";
+//         }
+//     }
+//     return string;
+// }
+// btn2.addEventListener('click', () => {
+//     a = +document.querySelector(".a").value;
+//     b = +document.querySelector(".b").value;
+//     console.log(numbers(a, b));
+// });
 
-//3
-const btn3 = document.querySelector(".btn3");
-function randArray(k) {
-    let arr2 = [];
-    for (let i = 0; i <= k; ++i) {
-        arr2.push(Math.round(Math.random() * 500));
-    }
-    return arr2;
-}
-btn3.addEventListener('click', () => {
-    k = +document.querySelector(".k").value;
-    console.log(randArray(k));
-});
+// //3
+// const btn3 = document.querySelector(".btn3");
+// function randArray(k) {
+//     let arr2 = [];
+//     for (let i = 0; i <= k; ++i) {
+//         arr2.push(Math.round(Math.random() * 500));
+//     }
+//     return arr2;
+// }
+// btn3.addEventListener('click', () => {
+//     k = +document.querySelector(".k").value;
+//     console.log(randArray(k));
+// });
 
-//4
-const btn4 = document.querySelector(".btn4");
-const arr3 = [5, 3, 4, 5, 6, 7, 3];
-const arr4 = compact(arr3);
-function compact(arr3) {
-    let arr5 = [];
-    for (let i = 0; i < arr3.length; ++i) {
-        if (!arr5.includes(arr3[i])) {
-            arr5.push(arr3[i]);
-        }
-    }
-    return arr5;
-}
-btn4.addEventListener('click', () => {
-    console.log(arr4);
-});
+// //4
+// const btn4 = document.querySelector(".btn4");
+// const arr3 = [5, 3, 4, 5, 6, 7, 3];
+// const arr4 = compact(arr3);
+// function compact(arr3) {
+//     let arr5 = [];
+//     for (let i = 0; i < arr3.length; ++i) {
+//         if (!arr5.includes(arr3[i])) {
+//             arr5.push(arr3[i]);
+//         }
+//     }
+//     return arr5;
+// }
+// btn4.addEventListener('click', () => {
+//     console.log(arr4);
+// });
 
-//5
-const btn5 = document.querySelector(".btn5");
-let arr6 = [5, 'Limit', 12, 'a', '3', 99, 2, [2, 4, 3, '33', 'a', 'text'], 'strong', 'broun'];
-let arrNew = oneTypeArr(arr6);
-function oneTypeArr(arr6) {
-    let arr7 = [];
-    let arr8 = [];
-    let resultArr = [];
-    for (let i = 0; i < arr6.length; ++i) {
-        if (typeof arr6[i] === "string") {
-            arr7.push(arr6[i]);
-        }
-        else if (typeof arr6[i] === 'number') {
-            arr8.push(arr6[i]);
-        }
-        if (Array.isArray(arr6[i])) {
-            for (let j = 0; j < arr6[i].length; ++j)
-                if (typeof arr6[i][j] === "string") {
-                    arr7.push(arr6[i][j]);
-                }
-                else if (typeof arr6[i][j] === 'number') {
-                    arr8.push(arr6[i][j]);
-                }
-        }
-    }
+// //5
+// const btn5 = document.querySelector(".btn5");
+// let arr6 = [5, 'Limit', 12, 'a', '3', 99, 2, [2, 4, 3, '33', 'a', 'text'], 'strong', 'broun'];
+// let arrNew = oneTypeArr(arr6);
+// function oneTypeArr(arr6) {
+//     let arr7 = [];
+//     let arr8 = [];
+//     let resultArr = [];
+//     for (let i = 0; i < arr6.length; ++i) {
+//         if (typeof arr6[i] === "string") {
+//             arr7.push(arr6[i]);
+//         }
+//         else if (typeof arr6[i] === 'number') {
+//             arr8.push(arr6[i]);
+//         }
+//         if (Array.isArray(arr6[i])) {
+//             for (let j = 0; j < arr6[i].length; ++j)
+//                 if (typeof arr6[i][j] === "string") {
+//                     arr7.push(arr6[i][j]);
+//                 }
+//                 else if (typeof arr6[i][j] === 'number') {
+//                     arr8.push(arr6[i][j]);
+//                 }
+//         }
+//     }
 
-    resultArr.push(arr7);
-    resultArr.push(arr8);
-    return resultArr;
-}
+//     resultArr.push(arr7);
+//     resultArr.push(arr8);
+//     return resultArr;
+// }
 
-btn5.addEventListener('click', () => {
-    console.log(arrNew);
-});
+// btn5.addEventListener('click', () => {
+//     console.log(arrNew);
+// });
 
-//6
-const btn6 = document.querySelector(".btn6");
-function calc(a, b, op) {
-    if (typeof a === "number" && typeof b === "number" && typeof op === "number") {
-        switch (op) {
-            case 1:
-                return a - b;
-            case 2:
-                return a * b;
-            case 3:
-                return a / b;
-            default:
-                return a + b;
-        }
-    }
-}
+// //6
+// const btn6 = document.querySelector(".btn6");
+// function calc(a, b, op) {
+//     if (typeof a === "number" && typeof b === "number" && typeof op === "number") {
+//         switch (op) {
+//             case 1:
+//                 return a - b;
+//             case 2:
+//                 return a * b;
+//             case 3:
+//                 return a / b;
+//             default:
+//                 return a + b;
+//         }
+//     }
+// }
 
-btn6.addEventListener('click', () => {
-    a = +document.querySelector(".a-value").value;
-    b = +document.querySelector(".b-value").value;
-    op = +document.querySelector(".op").value;
-    console.log(calc(a, b, op));
-});
+// btn6.addEventListener('click', () => {
+//     a = +document.querySelector(".a-value").value;
+//     b = +document.querySelector(".b-value").value;
+//     op = +document.querySelector(".op").value;
+//     console.log(calc(a, b, op));
+// });
 
-//7
-const btn7 = document.querySelector(".btn7")
-const resultTask7_1 = findUnique([1, 2, 3, 4, 5, 6, 7, 8]);
-const resultTask7_2 = findUnique([1, 2, 2, 3, 4, 5, 6, 7, 8]);
-function findUnique(arr9) {
+// //7
+// const btn7 = document.querySelector(".btn7")
+// const resultTask7_1 = findUnique([1, 2, 3, 4, 5, 6, 7, 8]);
+// const resultTask7_2 = findUnique([1, 2, 2, 3, 4, 5, 6, 7, 8]);
+// function findUnique(arr9) {
 
-    let obj = arr9.reduce((key, num) => {
-        if (!key[num]) {
-            key[num] = 1;
-            return key;
-        } else {
-            key[num] += 1;
-            return key;
-        }
-    }, {});
-    for (let index in obj) {
-        if (obj[index] > 1) {
-            return false;
-        }
+//     let obj = arr9.reduce((key, num) => {
+//         if (!key[num]) {
+//             key[num] = 1;
+//             return key;
+//         } else {
+//             key[num] += 1;
+//             return key;
+//         }
+//     }, {});
+//     for (let index in obj) {
+//         if (obj[index] > 1) {
+//             return false;
+//         }
 
-    }
-    return true
-}
+//     }
+//     return true
+// }
 
-btn7.addEventListener('click', () => {
-    console.log(resultTask7_1);
-    console.log(resultTask7_2);
-});
+// btn7.addEventListener('click', () => {
+//     console.log(resultTask7_1);
+//     console.log(resultTask7_2);
+// });
 
-//Ускладнена задача
-const btn8 = document.querySelector(".btn8")
-const tom = create("pass_for_Tom");
-function create(str) {
-    return function (newStr) {
-        if (newStr === str) {
-            return true;
-        }
-        return false;
-    }
-}
+// //Ускладнена задача
+// const btn8 = document.querySelector(".btn8")
+// const tom = create("pass_for_Tom");
+// function create(str) {
+//     return function (newStr) {
+//         if (newStr === str) {
+//             return true;
+//         }
+//         return false;
+//     }
+// }
 
-btn8.addEventListener('click', () => {
-    console.log(tom("pass_for_Tom"));
-    console.log(tom("pass_for_tom"));
-});
+// btn8.addEventListener('click', () => {
+//     console.log(tom("pass_for_Tom"));
+//     console.log(tom("pass_for_tom"));
+// });
 
--- task-4/script-4.js
+++ task-4/script-4.js
@@ -1,88 +1,166 @@
 //1
 const btn9 = document.querySelector(".btn9");
-const first = document.querySelector(".first");
-const second = document.querySelector(".second");
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function sumSliceArray(arr, first, second) {
     for (let item of arr) {
         if (isNaN(item)) {
-            throw TypeError("Value in array is not a number")
+            throw TypeError(":Value in array is not a number")
         }
     }
-    if (isNaN(first, second)) {
-        throw new TypeError("Second and third arguments are not a numbers!");
-    }
-    else if (!first || !second || !arr) {
-        throw new Error("Please, enter all of arguments")
-    }
-    else if (!Array.isArray(arr)) {
-        throw new TypeError("First argument is not an array!");
-    }
-    else if (first >= arr.length || second >= arr.length) {
-        throw new RangeError("Value of second and third arguments are out of range");
+    if (isNaN(first) || isNaN(second)) {
+        throw new TypeError(":Second and third arguments are not a numbers!");
     }
 
-    let sum = arr[first] + arr[second];
-    return sum;
+    else if (!Array.isArray(arr)) {
+        throw new TypeError(":First argument is not an array!");
+    }
+    else if (first >= arr.length || second >= arr.length || first < 0 || second < 0) {
+        throw new RangeError(":Value of second and third arguments are out of range");
+    }
+
+    return arr[first] + arr[second];
 }
 btn9.addEventListener('click', () => {
-    const firstInput = first.value;
-    const secondInput = second.value;
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const first = +document.querySelector(".first").value;
+    const second = +document.querySelector(".second").value;
 
     try {
-    const result = sumSliceArray(arr, firstInput, secondInput);
-    console.log(result);
+        const result = sumSliceArray(arr, first, second);
+        console.log(result);
     }
-    catch(error){
-    console.error(error.message);
+    catch (error) {
+        console.error(error.name, error.message);
     }
 });
 
 //2
 const btn10 = document.querySelector(".btn10");
 function checkAge(name, age, status) {
-    try {
-        name = document.querySelector(".user-name").value;
-        age = document.querySelector(".user-age").value;
-        status = document.querySelector(".user-status").value;
-    }
-    catch (error) {
-        console.error(error.message);
-    }
+
     if (!name) {
 
         alert(" The field is empty! Please enter your name");
-        throw new Error("The field user-name is empty");
+        throw new Error(":The field user-name is empty");
     }
     else if (!age) {
         alert("The field is empty! Please enter your age");
-        throw new Error("The field user-age is empty");
+        throw new Error(":The field user-age is empty");
     }
     else if (!status) {
         alert("The field is empty! Please enter your status");
-        throw new Error("The field user-status is empty");
+        throw new Error(":The field user-status is empty");
     }
     else if (age < 18 || age > 70) {
         alert("You are too old or too young to watch this movie!!!");
-        throw new RangeError("Age of user is out of range");
+        throw new RangeError(":Age of user is out of range");
     }
     else if (isNaN(age)) {
-        throw new TypeError("Field age is not a number");
+        throw new TypeError(":Field age is not a number");
     }
     else if (status !== "admin" && status !== "Mike") {
-        throw new EvalError("Status input is incorrect");
+        throw new EvalError(":Status input is incorrect");
     }
     alert("You can watch films at this site");
 
 }
 
-btn10.onclick = checkAge;
+btn10.addEventListener('click', () => {
+    const name = document.querySelector(".user-name").value;
+    const age = +document.querySelector(".user-age").value;
+    const status = document.querySelector(".user-status").value;
+
+    try {
+        const result = checkAge(name, age, status);
+    }
+    catch (error) {
+        console.error(error.name, error.message);
+    }
+});
 
 
 //3
 const btn11 = document.querySelector(".btn11");
 function calcRectangleArea(width, height) {
-    width = document.querySelector(".width");
-    height = document.querySelector(".height");
-    let rectangleArea = width * height;
+
+    if (width === 0 || height <= 0) {
+        throw new RangeError(":Incorrect input");
+    }
+    else if (isNaN(width) || isNaN(height)) {
+        throw new TypeError(":Values must be a number");
+    }
+    return width * height;
 }
+btn11.addEventListener('click', () => {
+    const width = +document.querySelector(".width").value;
+    const height = document.querySelector(".height").value;
+
+    try {
+        const result = calcRectangleArea(width, height);
+        console.log(result);
+    }
+    catch (error) {
+        console.error(error.name, error.message);
+    }
+});
+
+//4
+const btn12 = document.querySelector(".btn12");
+class MonthException {
+
+    constructor(message) {
+        this.message = message;
+        this.name = "MonthException";
+    }
+}
+
+function showMonthName(month) {
+    const monthInYear = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    if (month < 1 || month > 12) {
+        throw new MonthException(':Incorrect month number');
+    }
+
+    return monthInYear[month - 1];
+}
+
+
+btn12.addEventListener('click', () => {
+    const month = +document.querySelector(".month").value;
+    try {
+        console.log(showMonthName(month));  // May
+    }
+
+    catch (error) {
+        if (error instanceof MonthException) {
+            console.error(error.name, error.message);
+        }
+    }
+});
+
+const btn13 = document.querySelector(".btn13");
+
+function showUser(id) {
+    if (id < 0) {
+        throw new Error('ID must not be negative: ' + id);
+    }
+    return { id: id };
+}
+
+function showUsers(ids) {
+    const arr = [];
+    for (let item of ids) {
+
+        try {
+          arr.push(showUser(item));
+        }
+
+        catch (error) {
+            console.error(error.name, error.message);
+        }
+    }
+    return arr;
+}
+
+btn13.addEventListener('click', () => {
+    console.log(showUsers([7, -12, 44, 22]));
+});
+
