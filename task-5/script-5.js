59f4cf4880f16301e83252a7a7c35ec841cf6d86
VoltStudent <volt251988@gmail.com>
2023-05-04 11:01:36 +0300

    task-6

--- task-5/script-5.js
+++ task-5/script-5.js
@@ -1,192 +1,192 @@
-//1
-class Circle {
-    constructor(x, y, radius) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-    }
-    circleLength() {
-        let circleLength = 2 * Math.PI * this.radius;
-        return circleLength.toFixed(3);
-    }
-    static circleLength2(radius) {
-        let circleLength = 2 * Math.PI * radius;
-        return circleLength.toFixed(3);
-    }
-    copyCircle() {
-        return new Circle(this.x, this.y, this.radius);
-    }
-    static copyCircle2(x, y, radius) {
-        return new Circle(x, y, radius);
-    }
-    standInCircle(a, b) {
-        let standInCircle = Math.sqrt((a - this.x) ** 2 + (b - this.y) ** 2);
-        return standInCircle.toFixed(3) <= this.radius;
-    }
-    circleToString() {
-        return `Circle - x=${this.x}, y=${this.y}, radius=${this.radius}`
-    }
-}
+// //1
+// class Circle {
+//     constructor(x, y, radius) {
+//         this.x = x;
+//         this.y = y;
+//         this.radius = radius;
+//     }
+//     circleLength() {
+//         let circleLength = 2 * Math.PI * this.radius;
+//         return circleLength.toFixed(3);
+//     }
+//     static circleLength2(radius) {
+//         let circleLength = 2 * Math.PI * radius;
+//         return circleLength.toFixed(3);
+//     }
+//     copyCircle() {
+//         return new Circle(this.x, this.y, this.radius);
+//     }
+//     static copyCircle2(x, y, radius) {
+//         return new Circle(x, y, radius);
+//     }
+//     standInCircle(a, b) {
+//         let standInCircle = Math.sqrt((a - this.x) ** 2 + (b - this.y) ** 2);
+//         return standInCircle.toFixed(3) <= this.radius;
+//     }
+//     circleToString() {
+//         return `Circle - x=${this.x}, y=${this.y}, radius=${this.radius}`
+//     }
+// }
 
-const circle1 = new Circle(2, 5, 6);
-console.log(circle1.circleLength());
-console.log(Circle.circleLength2(7));
-console.log(circle1.copyCircle());
-console.log(Circle.copyCircle2(2, 4, 7));
-console.log(circle1.standInCircle(9, 68));
-console.log(circle1.circleToString());
+// const circle1 = new Circle(2, 5, 6);
+// console.log(circle1.circleLength());
+// console.log(Circle.circleLength2(7));
+// console.log(circle1.copyCircle());
+// console.log(Circle.copyCircle2(2, 4, 7));
+// console.log(circle1.standInCircle(9, 68));
+// console.log(circle1.circleToString());
 
-//2
-function propsCount(currentObject) {
-    return Object.keys(currentObject).length
-}
-let mentor = {
-    course: "JS fundamental",
-    duration: 3,
-    direction: "web-development"
-};
-console.log(propsCount(mentor));
+// //2
+// function propsCount(currentObject) {
+//     return Object.keys(currentObject).length
+// }
+// let mentor = {
+//     course: "JS fundamental",
+//     duration: 3,
+//     direction: "web-development"
+// };
+// console.log(propsCount(mentor));
 
-//3
+// //3
 
-class Person {
-    constructor(name, surname) {
-        this.name = name;
-        this.surname = surname;
-    }
-    showFullName() {
-        return (`${this.name} ${this.surname}`);
-    }
-}
+// class Person {
+//     constructor(name, surname) {
+//         this.name = name;
+//         this.surname = surname;
+//     }
+//     showFullName() {
+//         return (`${this.name} ${this.surname}`);
+//     }
+// }
 
-class Student extends Person {
-    constructor(name, surname, year) {
-        super(name, surname);
-        this.year = year;
-    }
-    showFullName(midleName) {
-        return (`${super.showFullName()} ${midleName}`);
-    }
-    showCourse() {
-        const thisYear = new Date().getFullYear();
-        const thisMonth = new Date().getMonth();
-        if (thisMonth >= 9) {
-            return thisYear - this.year + 1;
-        }
-        return thisYear - this.year;
-    }
-}
-const stud1 = new Student("Petro", "Petrenko", 2019);
-console.log(stud1.showFullName("Petrovych"));
-console.log("Current course: " + stud1.showCourse());
+// class Student extends Person {
+//     constructor(name, surname, year) {
+//         super(name, surname);
+//         this.year = year;
+//     }
+//     showFullName(midleName) {
+//         return (`${super.showFullName()} ${midleName}`);
+//     }
+//     showCourse() {
+//         const thisYear = new Date().getFullYear();
+//         const thisMonth = new Date().getMonth();
+//         if (thisMonth >= 9) {
+//             return thisYear - this.year + 1;
+//         }
+//         return thisYear - this.year;
+//     }
+// }
+// const stud1 = new Student("Petro", "Petrenko", 2019);
+// console.log(stud1.showFullName("Petrovych"));
+// console.log("Current course: " + stud1.showCourse());
 
-//4
-const btn14 = document.querySelector(".marker-btn");
-const btn15 = document.querySelector(".markerfill-btn")
-const markerText = document.querySelector(".marker-text");
-const markerInput = document.querySelector(".marker")
-class Marker {
-    constructor(color, inkAmount) {
-        this.color = color;
-        this.inkAmount = inkAmount;
-    }
-    markerPrint(text) {
-        let newInkAmount = this.inkAmount;
-        for (let i = 0; i < text.length; i++) {
-            if (text[i] !== " ")
-                newInkAmount -= 0.5;
-        }
-        return newInkAmount + '%';
-    }
-}
+// //4
+// const btn14 = document.querySelector(".marker-btn");
+// const btn15 = document.querySelector(".markerfill-btn")
+// const markerText = document.querySelector(".marker-text");
+// const markerInput = document.querySelector(".marker")
+// class Marker {
+//     constructor(color, inkAmount) {
+//         this.color = color;
+//         this.inkAmount = inkAmount;
+//     }
+//     markerPrint(text) {
+//         let newInkAmount = this.inkAmount;
+//         for (let i = 0; i < text.length; i++) {
+//             if (text[i] !== " ")
+//                 newInkAmount -= 0.5;
+//         }
+//         return newInkAmount + '%';
+//     }
+// }
 
-class MarkerRefill extends Marker {
-    constructor(color, inkAmount, refillValue) {
-        super(color, inkAmount);
-        this.refillValue = refillValue;
-    }
-    markerFill() {
-        this.inkAmount += this.refillValue;
-        return this.inkAmount + '%';
-    }
-}
-
-
-const marker1 = new Marker('green', 60);
-console.log(marker1.markerPrint('text    3sd'));
-const marker1fill = new MarkerRefill('green', 60, 25)
-console.log(marker1fill.markerFill());
-
-btn14.addEventListener('click', () => {
-    let outputMarker = markerInput.value;
-    markerText.innerHTML = outputMarker;
-    markerText.style.color = marker1.color;
-    markerText.style.opacity = marker1.markerPrint(outputMarker);
-});
-
-btn15.addEventListener('click', () => {
-    let outputMarker = markerInput.value;
-    markerText.innerHTML = outputMarker;
-    markerText.style.color = marker1.color;
-    markerText.style.opacity = marker1fill.markerFill(outputMarker);
-});
+// class MarkerRefill extends Marker {
+//     constructor(color, inkAmount, refillValue) {
+//         super(color, inkAmount);
+//         this.refillValue = refillValue;
+//     }
+//     markerFill() {
+//         this.inkAmount += this.refillValue;
+//         return this.inkAmount + '%';
+//     }
+// }
 
 
-//5
+// const marker1 = new Marker('green', 60);
+// console.log(marker1.markerPrint('text    3sd'));
+// const marker1fill = new MarkerRefill('green', 60, 25)
+// console.log(marker1fill.markerFill());
 
-class Worker {
-    #experience;
-    constructor(fullName, dayRate, workingDays) {
-        this.fullName = fullName;
-        this.dayRate = dayRate;
-        this.workingDays = workingDays;
-        this.#experience = 1.2;
-    }
+// btn14.addEventListener('click', () => {
+//     let outputMarker = markerInput.value;
+//     markerText.innerHTML = outputMarker;
+//     markerText.style.color = marker1.color;
+//     markerText.style.opacity = marker1.markerPrint(outputMarker);
+// });
 
-    showSalary() {
-        let salary = this.dayRate * this.workingDays;
-        // console.log(`${ this.fullName} salary: ${salary}`);
-        return salary;
-    }
-    showSalaryWithExperience() {
-        let newSalary = this.dayRate * this.workingDays * this.#experience;
-        // console.log(`${ this.fullName} salary: ${newSalary}`);
-        return newSalary;
-    }
+// btn15.addEventListener('click', () => {
+//     let outputMarker = markerInput.value;
+//     markerText.innerHTML = outputMarker;
+//     markerText.style.color = marker1.color;
+//     markerText.style.opacity = marker1fill.markerFill(outputMarker);
+// });
 
 
-    get experience() {
-        return this.#experience;
-    }
+// //5
 
-    set experience(value) {
-        this.#experience = value;
-    }
-}
+// class Worker {
+//     #experience;
+//     constructor(fullName, dayRate, workingDays) {
+//         this.fullName = fullName;
+//         this.dayRate = dayRate;
+//         this.workingDays = workingDays;
+//         this.#experience = 1.2;
+//     }
 
-let worker1 = new Worker("John Johnson", 20, 23);
-let worker2 = new Worker("Tom Tomson", 48, 22);
-let worker3 = new Worker("Andy Ander", 29, 23);
+//     showSalary() {
+//         let salary = this.dayRate * this.workingDays;
+//         // console.log(`${ this.fullName} salary: ${salary}`);
+//         return salary;
+//     }
+//     showSalaryWithExperience() {
+//         let newSalary = this.dayRate * this.workingDays * this.#experience;
+//         // console.log(`${ this.fullName} salary: ${newSalary}`);
+//         return newSalary;
+//     }
 
-let workers = [worker1, worker2, worker3];
 
-for(let item of workers) {
-    console.log(item.fullName);
-    console.log(`${item.fullName} salary: ${item.showSalary()}`);
-    console.log("New experience: " + item.experience);
-    console.log(`${item.fullName} salary: ${item.showSalaryWithExperience()}`);
-    item.experience = 1.5;
-    console.log("New experience: " + item.experience);
-    console.log(`${item.fullName} salary: ${item.showSalaryWithExperience()}`);
-}
+//     get experience() {
+//         return this.#experience;
+//     }
 
-workers.sort((a, b) =>{
-    a.showSalaryWithExperience(); - b.showSalaryWithExperience();;
-});
-console.log("Sorted salary:");
-for (let worker of workers) {
-  console.log(`${worker.fullName}: ${worker.showSalaryWithExperience()}`);
-}
+//     set experience(value) {
+//         this.#experience = value;
+//     }
+// }
+
+// let worker1 = new Worker("John Johnson", 20, 23);
+// let worker2 = new Worker("Tom Tomson", 48, 22);
+// let worker3 = new Worker("Andy Ander", 29, 23);
+
+// let workers = [worker1, worker2, worker3];
+
+// for(let item of workers) {
+//     console.log(item.fullName);
+//     console.log(`${item.fullName} salary: ${item.showSalary()}`);
+//     console.log("New experience: " + item.experience);
+//     console.log(`${item.fullName} salary: ${item.showSalaryWithExperience()}`);
+//     item.experience = 1.5;
+//     console.log("New experience: " + item.experience);
+//     console.log(`${item.fullName} salary: ${item.showSalaryWithExperience()}`);
+// }
+
+// workers.sort((a, b) =>{
+//     a.showSalaryWithExperience(); - b.showSalaryWithExperience();;
+// });
+// console.log("Sorted salary:");
+// for (let worker of workers) {
+//   console.log(`${worker.fullName}: ${worker.showSalaryWithExperience()}`);
+// }
     
 
 
--- task-6/script-6.js
+++ task-6/script-6.js
@@ -2,8 +2,8 @@
 
 const list = document.getElementById('list');
 const li = list.getElementsByTagName('li');
-const result = li[0].textContent + ',' + li[4].textContent + ',' + li[1].textContent + ',' + li[3].textContent + ',' + li[2].textContent;
-alert(result); 
+let result = li[0].textContent + ',' + li[4].textContent + ',' + li[1].textContent + ',' + li[3].textContent + ',' + li[2].textContent;
+alert(result)
 
 //2
 
@@ -22,7 +22,7 @@ myList.style.display = 'flex';
 myList.style.flexDirection = 'row';
 
 const invisibleText = document.getElementsByTagName('span');
-invisibleText[0].style.display = 'none';
+invisibleText[0].style.opacity = '0%';
 
 //3
 const spanTag = document.querySelector('span');
@@ -37,3 +37,59 @@ paragrapfTag.textContent = 'First paragraph';
 divTag.append(paragrapfTag);
 
 //4
+const form = document.querySelector('fieldset')
+
+const fullName = form.elements.fio;
+const phone = form.elements.phone;
+const birthday = form.elements.birthday;
+const email = form.elements.email;
+const submitBtn = document.querySelector('.btn');
+const outField = document.querySelector('.out');
+
+submitBtn.addEventListener('click', () => {
+    outField.innerHTML = `${fullName.getAttribute('data-form')}: ${fullName.value}; <br> 
+                          ${phone.getAttribute('data-form')}: ${phone.value}; <br>
+                          ${birthday.getAttribute('data-form')}: ${birthday.value}; <br>
+                          ${email.getAttribute('data-form')}: ${email.value};`
+}
+);
+
+//5
+const box = document.querySelector('.box')
+const circle = document.getElementsByClassName('circle');
+const animArr = [];
+for (let item of circle) {
+    animArr.push(item.getAttribute('data-anim'));
+}
+
+for (i = 0; i < animArr.length; ++i) {
+    box.children[i].classList.add(`${animArr[i]}`);
+}
+
+//6
+
+const color = document.getElementsByClassName('color');
+const prices = document.querySelector('#outprice');
+const nikeImg = document.getElementsByClassName('shoe');
+const backgroundGrad = document.getElementsByClassName('gradient')
+for (let i = 0; i < color.length; ++i) {
+color[i].addEventListener('click', () => {
+    for (let j = 0; j < color.length; j++) {
+        color[j].classList.remove('active');
+    }
+    color[i].classList.add('active');
+    let price = color[i].getAttribute('data-price');
+    prices.innerHTML = `${price}.00 $`;
+    for (let j = 0; j < nikeImg.length; j++) {
+        nikeImg[j].classList.remove('show');
+    }
+    nikeImg[i].classList.add('show');
+    for (let j = 0; j < backgroundGrad.length; j++) {
+        backgroundGrad[j].classList.remove('second');
+    }
+    backgroundGrad[i].classList.add('second');
+})
+}
+
+
+
