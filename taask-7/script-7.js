4679cac80a1e31a1f0b3d0812e00df5c5e5e1317
VoltStudent <volt251988@gmail.com>
2023-05-08 19:49:53 +0300

    task 7

---
+++ taask-7/script-7.js
@@ -0,0 +1,127 @@
+//1
+const btn16 = document.querySelector('.btn16');
+btn16.addEventListener('click', () => {
+    const newWindow = window.open('', '_blank', 'width=300,height=300');
+    setTimeout(() => {
+        newWindow.resizeTo(500, 500);
+        setTimeout(() => {
+            newWindow.moveTo(200, 200);
+            setTimeout(() => {
+                newWindow.close();
+            }, 2000);
+        }, 2000);
+    }, 2000);
+})
+
+//2
+const btn17 = document.querySelector('#new-style');
+const newText = document.querySelector('#text');
+function changeCSS() {
+    newText.style.color = 'orange';
+    newText.style.fontFamily = 'Comic Sans MS';
+    newText.style.fontSize = '20px';
+}
+
+btn17.addEventListener('click', changeCSS);
+
+//3
+
+const btn18 = document.querySelector('#btn18');
+const btn19 = document.querySelector('#btn19');
+const btn20 = document.querySelector('#btn20');
+const link1 = document.querySelector('#link1');
+
+btn18.addEventListener('click', () => {
+    document.body.style.background = 'blue';
+});
+
+btn19.addEventListener('dblclick', () => {
+    document.body.style.background = 'pink';
+});
+
+btn20.addEventListener('mousedown', () => {
+    document.body.style.background = 'brown';
+});
+
+btn20.addEventListener('mouseup', () => {
+    document.body.style.background = 'white';
+});
+
+link1.addEventListener('mouseover', () => {
+    document.body.style.background = 'yellow';
+});
+
+link1.addEventListener('mouseout', () => {
+    document.body.style.background = 'white';
+});
+
+//4
+
+const selectNames = document.querySelector('select');
+const btn21 = document.querySelector('#btn21');
+btn21.addEventListener('click', () => {
+    selectNames.options[selectNames.selectedIndex].remove();
+});
+
+//5
+
+const btn22 = document.querySelector('#btn22');
+const out2 = document.querySelector('#out2');
+
+btn22.addEventListener('click', () => {
+    out2.innerHTML += 'I was pressed<br>';
+});
+
+btn22.addEventListener('mouseover', () => {
+    out2.innerHTML += 'Mouse on me!<br>';
+});
+
+btn22.addEventListener('mouseout', () => {
+    out2.innerHTML += 'Mouse is not on me!<br>'
+});
+
+
+//6
+
+const out3 = document.querySelector('#out3');
+
+window.onresize = () => out3.innerHTML = `Width: ${window.innerWidth}, Heigth: ${window.innerHeight}`;
+
+//7
+const country = document.querySelector('#country');
+const cities = document.querySelector('#cities');
+const out4 = document.querySelector('#out4');
+const countryCities = {
+    Ukraine: ['Kyiv', 'Chernivtsi', 'Kharkiv', 'Chernigiv', 'Mykolaiv', 'Kherson', 'Simferopil'],
+    USA: ['Washington', 'New-York', 'Boston', 'Chicago'],
+    Germany: ['Berlin', 'Hamburg', 'Munich'],
+};
+
+function addOptions(citiesNames) {
+    cities.innerHTML = '';
+    for (let city of citiesNames) {
+        let newOption = document.createElement('option');
+        newOption.value = city;
+        newOption.text = city;
+        cities.append(newOption);
+    }
+}
+
+addOptions(countryCities.Ukraine);
+country.addEventListener('change', () => {
+    switch (country.value) {
+        case 'ger':
+            addOptions(countryCities.Germany);
+            break;
+        case 'ukr':
+            addOptions(countryCities.Ukraine);
+            break;
+        case 'usa':
+            addOptions(countryCities.USA);
+            break;
+    }
+});
+
+cities.addEventListener('change', () => {
+    out4.innerHTML = cities.value
+})
--- task-6/script-6.js
+++ task-6/script-6.js
@@ -1,95 +1,94 @@
-//1
+// //1
 
-const list = document.getElementById('list');
-const li = list.getElementsByTagName('li');
-let result = li[0].textContent + ',' + li[4].textContent + ',' + li[1].textContent + ',' + li[3].textContent + ',' + li[2].textContent;
-alert(result)
+// const list = document.getElementById('list');
+// const li = list.getElementsByTagName('li');
+// let result = li[0].textContent + ',' + li[4].textContent + ',' + li[1].textContent + ',' + li[3].textContent + ',' + li[2].textContent;
+// alert(result)
 
-//2
+// //2
 
-const header = document.querySelector('h1');
-header.style.backgroundColor = 'green';
+// const header = document.querySelector('h1');
+// header.style.backgroundColor = 'green';
 
-const myDiv = document.getElementById('myDiv');
-myDiv.children[0].style.fontWeight = 900;
-myDiv.children[1].style.color = 'red';
-myDiv.children[2].style.textDecoration = 'underline';
-myDiv.children[3].style.fontStyle = 'italic';
+// const myDiv = document.getElementById('myDiv');
+// myDiv.children[0].style.fontWeight = 900;
+// myDiv.children[1].style.color = 'red';
+// myDiv.children[2].style.textDecoration = 'underline';
+// myDiv.children[3].style.fontStyle = 'italic';
 
-const myList = document.getElementById('myList');
-myList.style.listStyle = 'none';
-myList.style.display = 'flex';
-myList.style.flexDirection = 'row';
+// const myList = document.getElementById('myList');
+// myList.style.listStyle = 'none';
+// myList.style.display = 'flex';
+// myList.style.flexDirection = 'row';
 
-const invisibleText = document.getElementsByTagName('span');
-invisibleText[0].style.opacity = '0%';
+// const invisibleText = document.getElementsByTagName('span');
+// invisibleText[0].style.opacity = '0%';
 
-//3
-const spanTag = document.querySelector('span');
-const newBox = document.createElement('main');
-newBox.classList.add('mainClass', 'check', 'item');
-spanTag.after(newBox);
-const divTag = document.createElement('div');
-divTag.setAttribute('id', 'myDiv');
-newBox.append(divTag);
-const paragrapfTag = document.createElement('p');
-paragrapfTag.textContent = 'First paragraph';
-divTag.append(paragrapfTag);
+// //3
+// const spanTag = document.querySelector('span');
+// const newBox = document.createElement('main');
+// newBox.classList.add('mainClass', 'check', 'item');
+// spanTag.after(newBox);
+// const divTag = document.createElement('div');
+// divTag.setAttribute('id', 'myDiv');
+// newBox.append(divTag);
+// const paragrapfTag = document.createElement('p');
+// paragrapfTag.textContent = 'First paragraph';
+// divTag.append(paragrapfTag);
 
-//4
-const form = document.querySelector('fieldset')
+// //4
+// const form = document.querySelector('fieldset')
 
-const fullName = form.elements.fio;
-const phone = form.elements.phone;
-const birthday = form.elements.birthday;
-const email = form.elements.email;
-const submitBtn = document.querySelector('.btn');
-const outField = document.querySelector('.out');
+// const fullName = form.elements.fio;
+// const phone = form.elements.phone;
+// const birthday = form.elements.birthday;
+// const email = form.elements.email;
+// const submitBtn = document.querySelector('.btn');
+// const outField = document.querySelector('.out');
 
-submitBtn.addEventListener('click', () => {
-    outField.innerHTML = `${fullName.getAttribute('data-form')}: ${fullName.value}; <br> 
-                          ${phone.getAttribute('data-form')}: ${phone.value}; <br>
-                          ${birthday.getAttribute('data-form')}: ${birthday.value}; <br>
-                          ${email.getAttribute('data-form')}: ${email.value};`
-}
-);
+// submitBtn.addEventListener('click', () => {
+//     outField.innerHTML = `${fullName.getAttribute('data-form')}: ${fullName.value}; <br> 
+//                           ${phone.getAttribute('data-form')}: ${phone.value}; <br>
+//                           ${birthday.getAttribute('data-form')}: ${birthday.value}; <br>
+//                           ${email.getAttribute('data-form')}: ${email.value};`
+// }
+// );
 
-//5
-const box = document.querySelector('.box')
-const circle = document.getElementsByClassName('circle');
-const animArr = [];
-for (let item of circle) {
-    animArr.push(item.getAttribute('data-anim'));
-}
+// //5
+// const box = document.querySelector('.box')
+// const circle = document.getElementsByClassName('circle');
+// const animArr = [];
+// for (let item of circle) {
+//     animArr.push(item.getAttribute('data-anim'));
+// }
 
-for (i = 0; i < animArr.length; ++i) {
-    box.children[i].classList.add(`${animArr[i]}`);
-}
+// for (i = 0; i < animArr.length; ++i) {
+//     box.children[i].classList.add(`${animArr[i]}`);
+// }
 
-//6
+// //6
 
-const color = document.getElementsByClassName('color');
-const prices = document.querySelector('#outprice');
-const nikeImg = document.getElementsByClassName('shoe');
-const backgroundGrad = document.getElementsByClassName('gradient')
-for (let i = 0; i < color.length; ++i) {
-color[i].addEventListener('click', () => {
-    for (let j = 0; j < color.length; j++) {
-        color[j].classList.remove('active');
-    }
-    color[i].classList.add('active');
-    let price = color[i].getAttribute('data-price');
-    prices.innerHTML = `${price}.00 $`;
-    for (let j = 0; j < nikeImg.length; j++) {
-        nikeImg[j].classList.remove('show');
-    }
-    nikeImg[i].classList.add('show');
-    for (let j = 0; j < backgroundGrad.length; j++) {
-        backgroundGrad[j].classList.remove('second');
-    }
-    backgroundGrad[i].classList.add('second');
-})
-}
-
+// const color = document.getElementsByClassName('color');
+// const prices = document.querySelector('#outprice');
+// const nikeImg = document.getElementsByClassName('shoe');
+// const backgroundGrad = document.getElementsByClassName('gradient');
+// for (let i = 0; i < color.length; ++i) {
+// color[i].addEventListener('click', () => {
+//     for (let j = 0; j < color.length; j++) {
+//         color[j].classList.remove('active');
+//     }
+//     color[i].classList.add('active');
+//     let price = color[i].getAttribute('data-price');
+//     prices.innerHTML = `${price}.00 $`;
+//     for (let j = 0; j < nikeImg.length; j++) {
+//         nikeImg[j].classList.remove('show');
+//     }
+//     nikeImg[i].classList.add('show');
+//     for (let j = 0; j < backgroundGrad.length; j++) {
+//         backgroundGrad[j].classList.remove('second');
+//     }
+//     backgroundGrad[i].classList.add('second');
+// })
+// }
 
 
