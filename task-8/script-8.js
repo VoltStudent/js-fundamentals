6b33e6958cac6541b228a484367d97013b746d2d
VoltStudent <volt251988@gmail.com>
2023-05-10 23:14:00 +0300

    task8

--- task-8/script-8.js
+++ task-8/script-8.js
@@ -1,19 +1,19 @@
 //1
 const btn23 = document.querySelector('#btn23');
 const out5 = document.querySelector('#out5');
-const input = document.querySelector('#input')
+const input = document.querySelector('#input');
 function upperCase(word) {
-    let regexp = /^[A-Z]/
-    if(regexp.test(word)){
+    let regexp = /^[A-Z]/;
+    if (regexp.test(word)) {
         out5.innerHTML = "String's starts with uppercase character";
         out5.style.backgroundColor = 'transparent';
     }
-    else{
-    out5.innerHTML = "String's not starts with uppercase character";
-    out5.style.backgroundColor = '#FF7F50'
+    else {
+        out5.innerHTML = "String's not starts with uppercase character";
+        out5.style.backgroundColor = '#FF7F50';
     }
 }
-    
+
 btn23.addEventListener('click', () => {
     upperCase(input.value);
 });
@@ -25,14 +25,14 @@ const input2 = document.querySelector('#input2');
 const out6 = document.querySelector('#out6')
 
 function checkEmail(email) {
-    let regexp = /^[\w\.\-]{3,}@\w+\.\w+$/
-    if(regexp.test(email)){
+    let regexp = /^[\w\.\-]{3,}@\w+\.\w+$/;
+    if (regexp.test(email)) {
         out6.innerHTML = "Email is valid";
         out6.style.backgroundColor = 'transparent';
     }
-    else{
-    out6.innerHTML = "Email is invalid";
-    out6.style.backgroundColor = '#FF7F50'
+    else {
+        out6.innerHTML = "Email is invalid";
+        out6.style.backgroundColor = '#FF7F50';
     }
 }
 
@@ -46,7 +46,7 @@ const input3 = document.querySelector('#input3');
 const out7 = document.querySelector('#out7');
 
 function replaceWords(string) {
-    let regexp = /(\w+)\s(\w+)/
+    let regexp = /(\w+)\s(\w+)/;
     out7.innerHTML = string.replace(regexp, "$2, $1")
 }
 
@@ -61,14 +61,14 @@ const input4 = document.querySelector('#input4');
 const out8 = document.querySelector('#out8');
 
 function checkCard(num) {
-    let regexp = /^(\d{4}\-){3}\d{4}$/
-    if(regexp.test(num)){
+    let regexp = /^(\d{4}\-){3}\d{4}$/;
+    if (regexp.test(num)) {
         out8.innerHTML = "Card number is valid";
         out8.style.backgroundColor = 'transparent';
     }
-    else{
-    out8.innerHTML = "Card number is invalid";
-    out8.style.backgroundColor = '#FF7F50'
+    else {
+        out8.innerHTML = "Card number is invalid";
+        out8.style.backgroundColor = '#FF7F50';
     }
 }
 
@@ -80,17 +80,17 @@ btn26.addEventListener('click', () => {
 
 const btn27 = document.querySelector('#btn27');
 const input5 = document.querySelector('#input5');
-const out9 = document.querySelector('#out9')
+const out9 = document.querySelector('#out9');
 
 function checkNewEmail(email) {
-    let regexp = /^[a-zA-Z0-9]+\-?\w+@\w+\.\w+$/
-        if(regexp.test(email)){
+    let regexp = /^[a-zA-Z0-9]+\w*\-?\w*@\w+\.\w+$/;
+    if (regexp.test(email)) {
         out9.innerHTML = "Email is valid";
         out9.style.backgroundColor = 'transparent';
     }
-    else{
-    out9.innerHTML = "Email is invalid";
-    out9.style.backgroundColor = '#FF7F50'
+    else {
+        out9.innerHTML = "Email is invalid";
+        out9.style.backgroundColor = '#FF7F50';
     }
 }
 
@@ -102,17 +102,17 @@ btn27.addEventListener('click', () => {
 const btn28 = document.querySelector('#btn28');
 const input6 = document.querySelector('#input6');
 function checkLogin(login) {
-    let regexp = /^[a-z]([\d]\.?|[a-z]){1,9}$/gi;
-    let numMatch = (login.match(/\d\.\d|\d/g) ||[]).join(', ');
-    if(regexp.test(login)){
+    let regexp = /^[a-z](\d\.?|[a-z]){1,9}$/gi;
+    let numMatch = (login.match(/\d\.\d|\d/g) || []).join(', ');
+    if (regexp.test(login)) {
         out10.innerHTML = "Login is correct ";
-        out10.innerHTML +=`//${numMatch}`
+        out10.innerHTML += `//${numMatch}`
         out10.style.backgroundColor = 'transparent';
     }
-    else{
-    out10.innerHTML = "login is incorrect ";
-    out10.innerHTML += `//${numMatch}`
-    out10.style.backgroundColor = '#FF7F50'
+    else {
+        out10.innerHTML = "login is incorrect ";
+        out10.innerHTML += `//${numMatch}`
+        out10.style.backgroundColor = '#FF7F50';
     }
 }
 
