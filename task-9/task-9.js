e0a6755db507695a60f5a3acbe255d07b34b2c7e
VoltStudent <volt251988@gmail.com>
2023-05-23 12:38:48 +0300

    task-10

--- task-9/task-9.js
+++ task-9/task-9.js
@@ -1,31 +1,33 @@
-//1
-$('#btn29').on('click', () => {
-$('h2.head').css('background-color', 'green');
-$('h2.head .inner').css('font-size', '35px');
-});
-//2
+// //1
+// $('#btn29').on('click', () => {
+// $('h2.head').css('background-color', 'green');
+// $('h2.head .inner').css('font-size', '35px');
+// });
+// //2
 
-$('a[href^="https://"]').attr('target', '_blank');
+// $('a[href^="https://"]').attr('target', '_blank');
 
-//3
+// //3
 
-$('#btn30').on('click', () => {
-    $('h3').each(function()  {
-        let div = $(this).next('div');
-        $(this).before(div);
-    });
-});
+// $('#btn30').on('click', () => {
+//     $('h3').each(function()  {
+//         let div = $(this).next('div');
+//         $(this).before(div);
+//     });
+// });
 
-//4
+// //4
 
-const checkboxes = $("input[type='checkbox']");
+// const checkboxes = $("input[type='checkbox']");
 
-checkboxes.change(function() {
-    let selectedboxes = checkboxes.filter(":checked");
-    console.log(selectedboxes)
-    if (selectedboxes.length >= 3) {
-      checkboxes.not(":checked").attr("disabled", true);
-    } else {
-      checkboxes.attr("disabled", false);
-    }
-  });
+// checkboxes.change(function() {
+//     let selectedboxes = checkboxes.filter(":checked");
+//     console.log(selectedboxes)
+//     if (selectedboxes.length >= 3) {
+//       checkboxes.not(":checked").attr("disabled", true);
+//     } else {
+//       checkboxes.attr("disabled", false);
+//     }
+//   });
+
+  
